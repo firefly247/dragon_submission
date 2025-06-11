c3904d09d50ad8852e0cb5c9db33102b397a0c07
firefly247 <jangjg323@gmail.com>
2025-06-11 10:58:56 +0900

    process.py 수정

--- process.py
+++ process.py
@@ -1,12 +1,13 @@
 import re
 from typing import List, Union
-
 import sys
 from pathlib import Path
 sys.path.append(str(Path(__file__).parent / "dragon_baseline" / "src"))
 
 from dragon_baseline import DragonBaseline
-
+from pathlib import Path
+from dragon_eval import DragonEval
+from dragon_baseline import ensemble_predictions
 
 class DragonSubmission(DragonBaseline):
     def __init__(
@@ -61,9 +62,57 @@ class DragonSubmission(DragonBaseline):
         super().preprocess()
 
         # Uncomment the following lines to use the custom_text_cleaning function
-        # for df in [self.df_train, self.df_val, self.df_test]:
-        #     df[self.task.input_name] = df[self.task.input_name].map(self.custom_text_cleaning)
+        for df in [self.df_train, self.df_val, self.df_test]:
+            df[self.task.input_name] = df[self.task.input_name].map(self.custom_text_cleaning)
 
 
 if __name__ == "__main__":
-    DragonSubmission().process()
+    # DragonSubmission().process()
+
+    # Note: to debug (outside of Docker), you can set the input and output paths.
+
+    # 모델별 workdir
+    workdir1 = Path(f"/opt/app/model1")
+    workdir2 = Path(f"/opt/app/model2")
+    workdir3 = Path(f"/opt/app/model3")
+
+    # 저장 경로 설정
+    output_path_pred1 = Path(f"/output/model1")
+    output_path_pred2 = Path(f"/output/model2")
+    output_path_pred3 = Path(f"/output/model3")
+
+    # 모델 1
+    model1 = DragonSubmission(output_path=output_path_pred1, workdir=workdir1, model_name="joeranbosma/dragon-roberta-large-domain-specific")
+    model1.load()
+    model1.validate()
+    model1.analyze()
+    model1.preprocess()
+    model1.train()
+    pred1 = model1.predict(df=model1.df_test)
+    model1.save(pred1)
+
+    # 모델 2
+    model2 = DragonSubmission(output_path=output_path_pred2, workdir=workdir2, model_name="joeranbosma/dragon-bert-base-mixed-domain")
+    model2.load()
+    model2.validate()
+    model2.analyze()
+    model2.preprocess()
+    model2.train()
+    pred2 = model2.predict(df=model2.df_test)
+    model2.save(pred2)
+
+    # 모델 3
+    model3 = DragonSubmission(output_path=output_path_pred3, workdir=workdir3, model_name="joeranbosma/dragon-roberta-base-mixed-domain")
+    model3.load()
+    model3.validate()
+    model3.analyze()
+    model3.preprocess()
+    model3.train()
+    pred3 = model3.predict(df=model3.df_test)
+    model3.save(pred3)
+
+    # 앙상블
+    ensemble_pred = ensemble_predictions([pred1, pred2, pred3], model1.task.target.problem_type)
+    model_ensemble = DragonSubmission()
+    model_ensemble.save(ensemble_pred)
+    model_ensemble.verify_predictions()
